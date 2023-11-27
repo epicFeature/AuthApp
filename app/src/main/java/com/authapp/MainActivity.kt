2a490ef056f052c2aca1702bfa0324bc389d239e
mariasokolova0195@gmail.com <mariasokolova0195@gmail.com>
2023-11-27 23:43:29 +0300

    Каркас проекта создан

---
+++ app/src/main/java/com/authapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.authapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.authapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
